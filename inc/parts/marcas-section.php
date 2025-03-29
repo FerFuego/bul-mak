1d59604c15566709cc1e426aee149dad801afd60
Luciano Colmano <lcolmano@gmail.com>
2025-03-29 12:05:55 -0300

    Cambio ruta de imagenes en marcas-section

--- inc/parts/marcas-section.php
+++ inc/parts/marcas-section.php
@@ -4,97 +4,97 @@
             <div class="marcas__slider owl-carousel">
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/abrasivos-argentinos.jpeg" alt="abrasivos-argentinos">
+                        <img src="/img/marcas/abrasivos-argentinos.jpeg" alt="abrasivos-argentinos">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/barovo.jpeg" alt="barovo">
+                        <img src="/img/marcas/barovo.jpeg" alt="barovo">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/black-y-decker.jpeg" alt="black-y-decker">
+                        <img src="/img/marcas/black-y-decker.jpeg" alt="black-y-decker">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/crossmaster.jpeg" alt="crossmaster">
+                        <img src="/img/marcas/crossmaster.jpeg" alt="crossmaster">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/depascale.jpeg" alt="depascale">
+                        <img src="/img/marcas/depascale.jpeg" alt="depascale">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/dewalt.jpeg" alt="dewalt">
+                        <img src="/img/marcas/dewalt.jpeg" alt="dewalt">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/dowen-pagio.jpeg" alt="dowen-pagio">
+                        <img src="/img/marcas/dowen-pagio.jpeg" alt="dowen-pagio">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/dremel.jpeg" alt="dremel">
+                        <img src="/img/marcas/dremel.jpeg" alt="dremel">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/echo.jpeg" alt="echo">
+                        <img src="/img/marcas/echo.jpeg" alt="echo">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/ezeta.jpeg" alt="ezeta">
+                        <img src="/img/marcas/ezeta.jpeg" alt="ezeta">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/gridest.jpeg" alt="gridest">
+                        <img src="/img/marcas/gridest.jpeg" alt="gridest">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/husqvarna.jpeg" alt="husqvarna">
+                        <img src="/img/marcas/husqvarna.jpeg" alt="husqvarna">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/kushiro.jpeg" alt="kushiro">
+                        <img src="/img/marcas/kushiro.jpeg" alt="kushiro">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/miyawa.jpeg" alt="miyawa">
+                        <img src="/img/marcas/miyawa.jpeg" alt="miyawa">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/ombu.jpeg" alt="ombu">
+                        <img src="/img/marcas/ombu.jpeg" alt="ombu">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/oregon.jpeg" alt="oregon">
+                        <img src="/img/marcas/oregon.jpeg" alt="oregon">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/stanley.jpeg" alt="stanley">
+                        <img src="/img/marcas/stanley.jpeg" alt="stanley">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/udovo.jpeg" alt="udovo">
+                        <img src="/img/marcas/udovo.jpeg" alt="udovo">
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="marcas__item">
-                        <img src="/new/img/marcas/uranga.jpeg" alt="uranga">
+                        <img src="/img/marcas/uranga.jpeg" alt="uranga">
                     </div>
                 </div>
             </div>
